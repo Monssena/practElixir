3e05b1816688d40f77af57c2d67fdf7f4ed666c4
Alexey Subbotin <alesu1543@gmail.com>
2023-04-11 21:18:56 +0300

    fix insert

--- select.exs
+++ select.exs
@@ -3,11 +3,14 @@ Mix.install([:myxql, :plug, :plug_cowboy])
 
 # модуль для работы с базой данных.
 defmodule ExDataBase do
-  # функция инициализации.
-  def init(sQuery) do
+  def init_conn() do
 	children = [{MyXQL, hostname: "127.0.0.1", username: "root", password: "", database: "test", name: :mydb}]
 	opts = [strategy: :one_for_one, name: MyApp.Supervisor]
 	Supervisor.start_link(children, opts)
+  end
+  # функция инициализации.
+  def init(sQuery) do
+	init_conn()
     {:ok, result} = MyXQL.query(:mydb, sQuery)
 	result
   end
@@ -60,42 +63,61 @@ defmodule ExDataBase do
 	  to_string(a1)
 	end
   end
+  
+  # добавление одной строки при условии, если были параметры GET из формы.
+  def if_param_insert(s1, s2, s3) do
+    if s1 != nil && s2 != nil && s3 != nil do
+	  init_conn()
+	  {:ok, result} = MyXQL.query(:mydb, "INSERT INTO myarttable (text, description, keywords) VALUES (?, ?, ?)", [s1, s2, s3])
+	  ExDataBase.log("User added 1 row to table myarttable")
+      result
+	end
+  end
+  
+  # отображение событий от пользователей из браузера на экран.
+  def log(sLine) do
+	IO.puts to_string(NaiveDateTime.utc_now) <> " Event: " <> sLine
+  end
 end
 
-# для отладки.
-#result = ExDataBase.init("SELECT * FROM myarttable WHERE id>14 ORDER BY id DESC")
-#IO.puts ExDataBase.columns(result)
-#IO.puts ExDataBase.rows(result)
-#IO.puts ExDataBase.if_one_row("@ver564yrblabla", "@ver")
-
-
-  
-########################	
-
+# модуль для работы из браузера.
 defmodule MyPlug do
-  import Plug.Conn
+  use Plug.Router
+  plug :match
+  plug Plug.Parsers, parsers: [:urlencoded]
+  plug :dispatch
 
-  def init(options) do
-    # initialize options
-    options
+  # для совместимости с Plug.Router.
+  get "/search" do
+    IO.puts "/search is ok."
+	fetch_query_params(conn)
   end
 
   # функция выполняется в момент подключения из браузера.
   def call(conn, _opts) do
 
+    # добавление 1 строки из формы select.html.
+    conn1 = fetch_query_params(conn)
+    col1 = conn1.params["col1"]
+    col2 = conn1.params["col2"]
+    col3 = conn1.params["col3"]
+	# каждый параметр проверяется на nil.
+    ExDataBase.if_param_insert(col1, col2, col3)
+	
+	
     # открытие шаблона.
 	{:ok, file} = File.open("select.html", [:read, :utf8])
 	put_resp_content_type(conn, "text/plain")
-	sText = ""
+	sText = "" # инициализация строковой переменной.
 	# отправка сразу всего файла с данными из БД.
-	send_resp(conn, 200, read_file(file, conn, sText))
+	send_resp(conn, 200, read_file(file, sText))
   end
   
-  # сложение строки и послед.
+  # сложение строки и последовательности байт.
   def plusSrt(s1, a1) do
 	  s1 <> to_string(a1)
   end
-  # сложение двух строк, если нет ключ. слов.
+  # сложение двух строк, если нет ключевых слов.
   def plusSrtExcept(sMain, sNew, sTag1, sTag2) do
 	  if sNew =~ sTag1 || sNew =~ sTag2 do
 	    sMain
@@ -104,7 +126,7 @@ defmodule MyPlug do
 	  end
   end
   # чтение шаблона и отправка пользователю в браузер.
-  def read_file(file, conn, sText) do
+  def read_file(file, sText) do
 	aLine = IO.read(file, :line)
 	stLine = to_string(aLine)
 	
@@ -128,9 +150,9 @@ defmodule MyPlug do
 
 
 	  # чтение файла шаблона из select.html методом рекурсии, пока не :eof файла.
-	  read_file(file, conn, sText)
+	  read_file(file, sText)
 	else
-	  IO.puts "User is ok."
+	  ExDataBase.log("The user got a select.html page")
 	  # возврат обработанного шаблона в браузер пользователю.
 	  sText
     end
